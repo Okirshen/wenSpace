07e7b5be763a5e46e2fbc1b13042d6415159d00e
Ofri <okirshen@gmail.com>
2021-06-10 00:43:30 +0300

    added ssr fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,29 @@ import LaunchCard from '@/LaunchCard';
 import Launch from '@lib/Launch';
 import axios from 'axios';
 import styles from '@styles/styles.module.css'
-import React, { useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import moment from 'moment';
-// import styles from '../styles/Home.module.css';
 
-
-
-export default function Home() {
-	const [launches, setLaunches] = useState<Launch[]>([])
-
-	const fetchData = async () => {
-		console.log(moment().format('yyyy-MM-DDThh:mm:ss\Z'));
-
-	const { data } = await axios.get<{results: Launch[]}>(`https://lldev.thespacedevs.com/2.2.0/launch/?ordering=net&net__gt=${moment().format('yyyy-MM-DDThh:mm:ss')}Z`)
-		setLaunches(data.results as Launch[])
-		console.dir(data);
-
+interface Props {
+	launches: Launch[]
 }
 
-	useEffect(() => {
-		fetchData()
-	}, [])
-
+const index: FC<Props> = ({ launches }) => {
 	return <main className={styles.cards}>
 		{launches.map((launch) => (
 			<LaunchCard key={launch.id} launch={launch}/>
 		))}
 	</main>
+}
+
+export default index
+
+export async function getServerSideProps(context) {
+	console.log(moment().format('yyyy-MM-DDThh:mm:ss\Z'));
+
+	const { data } = await axios.get<{results: Launch[]}>(`${process.env.API}/launch/?ordering=net&net__gt=${moment().format('yyyy-MM-DDThh:mm:ss')}Z`)
+	console.dir(data);
+  return {
+    props: {launches: data.results},
+  }
 }
